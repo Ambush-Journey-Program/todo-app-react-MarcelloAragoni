4aa93452609f528e399651d5f77e08d4855497d4
Marcello <marcello_gp_@hotmail.com>
2022-12-22 18:04:51 -0300

    created checkbox state feature

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -2,21 +2,45 @@ import { FILTERS_VALUES } from '../../utilities/consts';
 import TodoItem from './ItemList';
 import * as S from './style';
 
-export default function TodoList({ itemList, filter = '' }) {
-  if (!itemList || itemList.length === 0) {
+export default function TodoList({ listItem, filter = '', handleUpdateList }) {
+  if (!listItem || listItem.length === 0) {
     return null;
+  }
+
+  function changeStatus(id, newStatus) {
+    // const itensUpdated = listItem.map((item) => {
+    //   if (item.id === id) {
+    //     return {
+    //       ...item,
+    //       status: newStatus
+    //     };
+    //   }
+    //   return item;
+    // });
+
+    const itensUpdated = listItem.map((item) =>
+      item.id === id ? { ...item, status: newStatus } : item
+    );
+
+    handleUpdateList(itensUpdated);
   }
 
   const shouldShowCompleteList = !filter || filter === FILTERS_VALUES.ALL;
 
   const filteredList = shouldShowCompleteList
-    ? itemList
-    : itemList.filter((item) => item.status === filter);
+    ? !listItem || listItem
+    : (!listItem || listItem).filter((item) => item.status === filter);
 
   return (
     <S.TodoList>
       {filteredList.map((item) => (
-        <TodoItem key={item.id} item={item.text} />
+        <TodoItem
+          key={item.id}
+          updateStatus={changeStatus}
+          id={item.id}
+          item={item.text}
+          status={item.status}
+        />
       ))}
     </S.TodoList>
   );
